f730e7116f9b5c5c1b15bcd17150692346d4507c
tuenut <unnetu@gmail.com>
2019-10-03 10:28:44 +0300

    add react

--- playitedit.py
+++ playitedit.py
@@ -1,4 +1,3 @@
-import frontend.mainwindow
 from os.path import basename
 
 from backend.logger import make_logger
@@ -9,4 +8,7 @@ if __name__ == '__main__':
     logger = make_logger(DEBUG)
     logger.log(DEBUG, '%s is started.' % basename(__file__))
 
-    frontend.mainwindow.MainAppWindow().root.mainloop()
+# todo: must run rest-api local server. flask?
+# todo: rest-api need for open files, get json structure, receive json and save files.
+# todo: I.e. that's interface between react in electron and backend.serializer.PlayItProject in python
+
